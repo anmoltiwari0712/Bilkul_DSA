6fe30b9a23e265aba59ac236b3db54e8d34198a1
Anmol Tiwari <oanmoltiwari7@gmail.com>
2025-08-23 22:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (45.41%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,19 +1,36 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         while (stones.size() > 1) {
-        // find two largest
-        int flargest = *max_element(stones.begin(), stones.end());
-        stones.erase(find(stones.begin(), stones.end(), flargest));
+            int n = stones.size();
+            int flargest = -1, slargest = -1; // store indices
 
-        int slargest = *max_element(stones.begin(), stones.end());
-        stones.erase(find(stones.begin(), stones.end(), slargest));
+            // find index of first largest and second largest
+            for (int i = 0; i < n; i++) {
+                if (flargest == -1 || stones[i] > stones[flargest]) {
+                    slargest = flargest;
+                    flargest = i;
+                } else if (slargest == -1 || stones[i] > stones[slargest]) {
+                    slargest = i;
+                }
+            }
 
-        // smash
-        if (flargest != slargest) {
-            stones.push_back(flargest - slargest);
+            int x = stones[flargest];
+            int y = stones[slargest];
+
+            // remove both elements safely
+            stones.erase(stones.begin() + flargest);
+            stones.erase(stones.begin() + (slargest > flargest ? slargest - 1 : slargest));
+
+            // smash and push back if not equal
+            if (x != y) {
+                stones.push_back(x - y);
+            }
         }
-    }
-    return stones.empty() ? 0 : stones[0];
+
+        return stones.empty() ? 0 : stones[0];
     }
 };
