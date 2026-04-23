63af03933f32b3ac0855e15f655c3d4a9eb6b36f
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-04-23 20:29:20 +0530

    Time: 3 ms (7.69%) | Memory: 9.7 MB (62.63%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int ncr(int n,int r){
+        int res=1;
+        for(int i=0;i<r;i++){
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        return res;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=0;i<numRows;i++){
+            vector<int> temp;
+            for(int j=0;j<=i;j++){
+                int val=ncr(i,j);
+                temp.push_back(val);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
