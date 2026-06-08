875255a3f0df1b0910fe93f169e541506d398115
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-06-08 18:07:33 +0530

    Time: 27 ms (5.72%) | Memory: 67.3 MB (5.19%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         int n=s.length();
-        int count=0;
-        string ans="";
+        int balance=0;
+        string res="";
         for(int i=0;i<n;i++){
-            if(s[i]==')'){
-                count++;
-            }
-            if(count!=0){
-                ans.push_back(s[i]);
-            }
             if(s[i]=='('){
-                count--;
+                if(balance>0){
+                    res=res+s[i];
+                }
+                balance++;
             }
-            
+            else{
+                balance--;
+                if(balance>0){
+                    res=res+s[i];
+                }
+            }
         }
-        return ans;
+        return res;
     }
 };
