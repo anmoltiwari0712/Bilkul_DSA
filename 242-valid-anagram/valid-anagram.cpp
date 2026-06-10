c6d032dce765a3fe723860b7d1fa1bf1de9f6794
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-06-10 23:04:24 +0530

    Time: 4 ms (34.32%) | Memory: 9.9 MB (25.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mpp1;
-        map<char,int> mpp2;
         int n1=s.length();
         int n2=t.length();
+
+        unordered_map<char,int> mpp1;
+        unordered_map<char,int> mpp2;
+
+
         if(n1!=n2){
             return false;
         }
+
         for(int i=0;i<n1;i++){
             mpp1[s[i]]++;
         }
+
         for(int i=0;i<n2;i++){
             mpp2[t[i]]++;
         }
-        for(int i=0;i<mpp1.size();i++){
-            if(mpp1[i]!=mpp2[i]){
-                return false;
-            }
-        }
-        return true;
 
+        if(mpp1==mpp2){
+            return true;
+        }
+
+        return false;
     }
 };
