2d47ff6b3992701728a11fbb2e09d2227eef2fc3
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-06-14 00:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.01%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -6,10 +6,17 @@ public:
         int sum=0;
         for(int i=0;i<n;i++){
             sum=sum+nums[i];
-            maxsum=max(maxsum,sum);
+
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+
             if(sum<0){
                 sum=0;
             }
+
+           
+
         }
         return maxsum;
     }
