09ffc0bcf3cf3564f58c704d9432435057b258cf
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-06-13 11:20:11 +0530

    Time: 3 ms (68.45%) | Memory: 10.9 MB (40.24%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n=s.length();
-        int maxlen=0;
         int left=0;
         int right=0;
+        int n=s.length();
+
         int freqarr[26]={0};
         int maxfreq=0;
+        int maxlen=0;
+
         while(right<n){
             freqarr[s[right]-'A']++;
-            // int window=right-left+1;
             maxfreq=max(maxfreq,freqarr[s[right]-'A']);
-            // int req=window-maxfreq;
+            // int window=right-left+1;
             while((right-left+1)-maxfreq>k){
-                freqarr[s[left] - 'A']--; 
+                freqarr[s[left]-'A']--;
                 left++;
             }
-            maxlen=max(maxlen,right-left+1);
+            maxlen=max(right-left+1,maxlen);
             right++;
         }
+
         return maxlen;
     }
 };
