dc9be3756dcbc1e8f1b190a471713e98f01e73b0
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-06-13 20:13:52 +0530

    Time: 186 ms (90.14%) | Memory: 10.2 MB (99.23%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,55 +1,52 @@
 class Solution {
 public:
+    bool dfs(vector<vector<char>>& board, string& word, int row, int col, int index){
 
-    bool dfs(vector<vector<char>>& board, string& word,
-             int row, int col, int index) {
-
-        // Entire word matched
         if(index == word.length()) {
+
             return true;
+
         }
 
-        // Out of bounds
-        if(row < 0 || row >= board.size() ||
-           col < 0 || col >= board[0].size()) {
+        if(row<0 || col<0 || row >= board.size() || col >= board[0].size()){
             return false;
         }
 
-        // Character mismatch
-        if(board[row][col] != word[index]) {
+        if(board[row][col]!=word[index]){
             return false;
         }
 
-        // Mark current cell as visited
-        char temp = board[row][col];
-        board[row][col] = '#';
+        char temp=board[row][col];
+        board[row][col]='#';
 
-        bool found =
-            dfs(board, word, row + 1, col, index + 1) ||  // Down
-            dfs(board, word, row - 1, col, index + 1) ||  // Up
-            dfs(board, word, row, col + 1, index + 1) ||  // Right
-            dfs(board, word, row, col - 1, index + 1);    // Left
+        bool found= dfs(board,word,row+1,col,index+1) || //down
+        dfs(board, word, row-1, col, index+1) || //up
+        dfs(board, word, row, col+1, index+1) || //right
+        dfs(board, word, row, col-1, index+1);  //left
 
-        // Backtrack
-        board[row][col] = temp;
+
+
+        board[row][col]=temp;
 
         return found;
+
+
+        
+
+
     }
-
     bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
 
-        int n = board.size();
-        int m = board[0].size();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-
-                if(dfs(board, word, i, j, 0)) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(dfs(board,word,i,j,0)){
                     return true;
                 }
             }
         }
-
         return false;
     }
 };
