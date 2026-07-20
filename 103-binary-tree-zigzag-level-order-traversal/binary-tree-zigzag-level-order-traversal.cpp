782d5b5adadf40fb4fcfaa888a569a3f4e689249
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-07-20 17:42:26 +0530

    Time: 2 ms (12.68%) | Memory: 15.2 MB (48.57%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,7 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-
         vector<vector<int>> result;
 
         if(root == NULL){
@@ -11,38 +21,32 @@ public:
         queue<TreeNode*> q;
         q.push(root);
 
-        bool leftorright = true;
+        bool flag=true;
 
         while(!q.empty()){
-
-            int size = q.size();
+            int size=q.size();
             vector<int> level;
-
-            for(int i = 0; i < size; i++){
-
-                TreeNode* node = q.front();
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
                 q.pop();
-
+                
                 level.push_back(node->val);
 
-                if(node->left != NULL){
+                if(node->left!=NULL){
                     q.push(node->left);
                 }
 
-                if(node->right != NULL){
+                if(node->right!=NULL){
                     q.push(node->right);
                 }
             }
-
-            if(!leftorright){
-                reverse(level.begin(), level.end());
+            if(!flag){
+                reverse(level.begin(),level.end());
             }
-
             result.push_back(level);
-
-            leftorright = !leftorright;
+            flag=!flag;
+            
         }
-
         return result;
     }
 };
