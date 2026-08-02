80bb3e68976ee1b71b0a3864f21be1bf6fe34b26
Anmol Tiwari <oanmoltiwari7@gmail.com>
2026-08-02 22:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (44.44%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,50 +12,44 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-
-        //col - row - sorted values
         map<int, map<int,multiset<int>>> nodes;
 
-        //(nodes,(col,row))
         queue<pair<TreeNode*, pair<int,int>>> q;
 
         q.push({root,{0,0}});
 
         while(!q.empty()){
-
-            TreeNode* currentnode=q.front().first;
+            TreeNode* currentNode = q.front().first;
             int currentcol=q.front().second.first;
             int currentrow=q.front().second.second;
 
+            nodes[currentcol][currentrow].insert(currentNode->val);
+
             q.pop();
 
-            nodes[currentcol][currentrow].insert(currentnode->val);
-
-            if(currentnode->left){
-                q.push({currentnode->left,{currentcol-1,currentrow+1}});
+            if(currentNode->left){
+                q.push({currentNode->left,{currentcol-1,currentrow+1}});
             }
 
-            if(currentnode->right){
-                q.push({currentnode->right,{currentcol+1,currentrow+1}});
+            if(currentNode->right){
+                q.push({currentNode->right,{currentcol+1,currentrow+1}});
             }
+
         }
 
         vector<vector<int>> ans;
 
-        //traverse every col
-        for(auto column: nodes){
+        for(auto column:nodes){
             vector<int> onecolumn;
-            
-            //Traverse every row in that col
+
             for(auto row: column.second){
 
-                for(auto value: row.second){
-                    onecolumn.push_back(value);
+                for(auto val: row.second){
+                    onecolumn.push_back(val);
                 }
             }
             ans.push_back(onecolumn);
         }
         return ans;
-
     }
 };
